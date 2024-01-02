bef07c072e9307b7fa085821485b20360a0c0c46
neda-pro <neda.pro@outlook.com>
2024-01-02 11:58:40 +0100

    user profile completed

---
+++ src/components/UserInfo.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Box, IconButton, Typography } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+
+const UserInfo = ({ title, value, noCap }) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          gap: 3,
+          textTransform: noCap ? "unset" : "capitalize",
+        }}
+      >
+        <Typography>{title}</Typography>
+        <Typography>{value}</Typography>
+      </Box>
+      <IconButton>
+        <EditIcon />
+      </IconButton>
+    </Box>
+  );
+};
+
+export default UserInfo;
--- src/routes/profile.jsx
+++ src/routes/profile.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { useUsers } from "../hooks/useUsers";
+import { Paper } from "@mui/material";
+import UserInfo from "../components/UserInfo";
 
 const Profile = () => {
-  return <div>profile</div>;
+  const {
+    data: userData,
+    isError: userIsError,
+    isLoading: userIsLoading,
+  } = useUsers();
+  if (userIsLoading) {
+    return null;
+  }
+  const { address, email, id, name, phone, username } = userData;
+  return (
+    <Paper sx={{ width: 500, ml: "auto", mr: "auto", mt: 5, p: 1 }}>
+      <UserInfo title={"User ID:"} value={username} />
+      <UserInfo
+        title={"User Name:"}
+        value={`${name.firstname} ${name.lastname}`}
+      />
+      <UserInfo noCap title={"Email:"} value={email} />
+      <UserInfo
+        title={"Address:"}
+        value={`${address.number}, ${address.street}, ${address.city}`}
+      />
+      <UserInfo title={"Phone:"} value={phone} />
+    </Paper>
+  );
 };
 
 export default Profile;
